0c6b9104e88ba18845f0125fdb278b95e4708dbe
ajay <ajaysunarthi@iitj.ac.in>
2017-01-07 23:23:50 +0530

    list everything in images dir

--- index.js
+++ index.js
@@ -2,11 +2,24 @@ var fs = require('fs');
 var http = require('http');
 var server = http.createServer();
 
-server.on('request',function(req,res) {
-   
-   res.write('Hello World');
-   res.end();
-   return;
+server.on('request', function(req, res) {
+
+    if (req.url === '/') {
+
+        var content = fs.readFileSync('list.html', 'utf8'),
+            list = '';
+
+        fs.readdirSync('images').forEach(function(d) {
+            list += '<li style="margin-bottom: 10px;"><a href="/' + d + '">' + d + '</a></li>';
+        });
+
+        content = content.replace(/{{items_list}}/g, list);
+        res.setHeader('content-type', 'text/html');
+        res.write(content);
+        res.end();
+        return;
+    }
+
 });
 
 server.listen(4000);
