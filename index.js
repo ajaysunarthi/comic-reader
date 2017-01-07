c7e8c4ced76d92e7e87da26df4b524267292dce2
ajay <ajaysunarthi@iitj.ac.in>
2017-01-08 01:10:50 +0530

    opens the first image of any directory inside images dir

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var fs = require('fs');
 var http = require('http');
 var server = http.createServer();
 var favicon = fs.readFileSync('favicon.ico');
+var authorizedExtensions = ['.jpg', '.jpeg', '.png'];
 
 server.on('request', function(req, res) {
 
@@ -34,6 +35,36 @@ server.on('request', function(req, res) {
         return;
     }
 
+    var content = fs.readFileSync('display.html', 'utf8');
+    var directory = req.url.split('/')[1];
+    var image = req.url.split('/')[2];
+    var current = 0,
+        i;
+    var _dirContents = fs.readdirSync('images/' + directory);
+    var dirContents = [];
+    _dirContents.forEach(function(f) {
+        var ext = f.split('.');
+        ext = '.' + ext[ext.length - 1];
+        if (authorizedExtensions.indexOf(ext) !== -1) { dirContents.push(f); }
+    });
+
+    if (image) {
+        for (i = 0; i < dirContents.length; i += 1) {
+            if (image === dirContents[i]) { current = i; }
+        }
+    }
+
+    var title = directory + ' - ' + (current + 1) + '/' + dirContents.length;
+    current = dirContents[current];
+
+    content = content.replace(/{{directory}}/g, directory);
+    content = content.replace(/{{current}}/g, current);
+    content = content.replace(/{{title}}/g, title);
+
+    res.setHeader('content-type', 'text/html');
+    res.write(content);
+    res.end();
+    return;
 
 });
 
